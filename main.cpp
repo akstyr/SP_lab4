c74d8e91a14251be8aa7317de65c97405a4b54ee
Kate <36703169+akstyr@users.noreply.github.com>
2020-06-10 09:54:59 +0700

    new main

--- main.cpp
+++ main.cpp
@@ -15,16 +15,18 @@ void proc_call(int decide)
     int local = 0, status;
     g_lock.lock();
 
-    pid_t pr;
-    pr = fork();
-    if(pr == 0)
+    pid_t pr; /*идентификатор процесса*/
+    pr = fork(); /*создание нового процесса*/
+    if(pr == 0) /*успешное создание?*/
     {
+		/*путь, указатели на аргументы, 0*/
         execl("secondary", " ", to_string(decide).c_str(), NULL);
     }
     g_lock.unlock();
+	/*приостанавливаем работу до завершения процесса*/
     wait(&status);
 }
- 
+
 int main(void)
 { 
     proc_call(1);
